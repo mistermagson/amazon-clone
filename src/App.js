8dabeb715e66ab40fc9c0da9ece2061ae12ff27e
Magson Martins Magalhaes <magson@gmail.com>
2020-10-15 15:04:06 -0400

    Creates Login.js and connects to Firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Home from "./Home";
 import Header from "./Header";
+import Login from "./Login";
+import {auth} from "./firebase-config"
 
-function App() {
+const App = () => {
+
+    useEffect( (dispatch) =>{
+            auth.onAuthStateChanged((authUser) => {
+                console.log("THE USER IS >>> ", authUser);
+
+                if (authUser) {
+                    // the user just logged in / the user was logged in
+
+                    dispatch({
+                        type: "SET_USER",
+                        user: authUser,
+                    });
+                } else {
+                    // the user is logged out
+                    dispatch({
+                        type: "SET_USER",
+                        user: null,
+                    });
+                }
+            });
+        }
+
+    ,[]);
+
   return (
     <div className="App">
         <Router>
             <Switch>
+                <Route path="/login">
+                    <Login />
+                </Route>
                 <Route path="/">
                     <Header />
                     <Home />
